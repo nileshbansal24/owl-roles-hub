93d8ad8578128adbf3068e60251eb639275373b4
Anony <73958752+celestix@users.noreply.github.com>
2026-02-01 05:24:09 +0530

    temp: use repo name as base path

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  base: '/owl-roles-hub/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
